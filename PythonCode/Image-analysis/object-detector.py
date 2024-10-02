623b2591d34328358f00d439538472e78e5c61ea
HectorMendezCIP <hector.mendez.fernandez@est.una.ac.cr>
2024-10-02 15:46:32 -0600

    Add in main - Az client
    Instantiate an Azure AI client

--- PythonCode/Image-analysis/object-detector.py
+++ PythonCode/Image-analysis/object-detector.py
@@ -9,3 +9,23 @@ import requests
 from azure.ai.vision.imageanalysis import ImageAnalysisClient
 from azure.ai.vision.imageanalysis.models import VisualFeatures
 from azure.core.credentials import AzureKeyCredential
+
+
+def main():
+    # Load the environment variables
+    load_dotenv()
+    # Get the endpoint and key from the environment variables
+    endpoint = os.getenv('ENDPOINT')
+    key = os.getenv('KEY')
+    
+    #get the image file from the command line arguments
+    image_file = "images/image1.jpg"
+    if len(sys.argv) == 2:
+        image_file = sys.argv[1]
+    # Reads the specified image file into a byte array.
+    with open(image_file, "rb") as f:
+        data = f.read()
+    # Instantiate an Azure AI client
+    az_client = ImageAnalysisClient(endpoint, AzureKeyCredential(key))
+    
+    
